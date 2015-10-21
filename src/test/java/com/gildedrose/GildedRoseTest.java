f2bf9d9032efe3a77b64a4683327ae65c36382ac
0xNaN <enzodicicco@gmail.com>
2015-10-21 15:08:51 +0200

    testing first feature

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,11 +8,29 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    public void 
+    quality_decrease_by_one_every_day() {
+    	Item[] items = new Item[] {new Item("fixme", 2, 10) };
+    	GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(9, app.items[0].quality);
     }
-
+    
+    @Test
+    public void 
+    sell_in_decrease_by_one_every_day() {
+    	Item[] items = new Item[] {new Item("fixme", 2, 10) };
+    	GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, app.items[0].sellIn);
+    }
+    
+    @Test
+    public void 
+    once_the_sell_by_date_has_passed_the_quality_degrades_twice_as_fast() {
+    	Item[] items = new Item[] {new Item("fixme", 0, 10) };
+    	GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+    }
 }
